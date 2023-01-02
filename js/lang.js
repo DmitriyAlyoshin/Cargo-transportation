4a055019ac49ba681cf37206709e9b1313e0988b
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-01-03 00:13:55 +0100

    Hero created

---
+++ js/lang.js
@@ -0,0 +1,37 @@
+const langArr = {
+    "header" :  {
+        "ru": "Слава Украине!",
+        "en": "Glory to Ukraine!",
+        "ua": "Слава Україні!",
+    }, 
+    "hero" :  {
+        "ru": "Грузоперевозки в Европу из Украины",
+        "en": "Cargo transportation to Europe from Ukraine",
+        "ua": "Вантажоперевезення в Європу з України",
+    }, 
+    "chip": {
+        "ru": "графический чип",
+        "en": "graphics processing unit",
+        "ua": "графічний чіп",
+    }, 
+    "memory": {
+        "ru": "объем памяти",
+        "en": "memory size",
+        "ua": "oбсяг пам'яті",
+    }, 
+    "memory-type": {
+        "ru": "тип памяти",
+        "en": "memory type",
+        "ua": "nип пам'яті",
+    }, 
+    "cool": {
+        "ru": "тип системы охлаждения",
+        "en": "сooling system type",
+        "ua": "тип системи охолодження",
+    }, 
+    "more": {
+        "ru": "детально о товаре",
+        "en": "more details",
+        "ua": "детально про товар",
+    }, 
+}
